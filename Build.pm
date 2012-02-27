c2514b3d125cd0c72454887a7070229c7bbca207
Christian Schneemann <schneemann@b1-systems.de>
2012-02-27 20:11:07 +0100

    changed sysroots into sysroot

--- Build.pm
+++ Build.pm
@@ -194,7 +194,7 @@ sub read_config {
   $config->{'repotype'} = [];
   $config->{'patterntype'} = [];
   $config->{'fileprovides'} = {};
-  $config->{'sysroots'} = [];
+  $config->{'sysroot'} = [];
   $config->{'crossbuild'} = 0;
   $config->{'hostsysroot'} = '';
   $config->{'targetsysroot'} = '';
